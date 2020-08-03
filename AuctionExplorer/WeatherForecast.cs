7e9974e9dbd7851c86618eff3f799f1bdbb35d9b
norbert-k <kakste.norberts@gmail.com>
2020-08-03 19:49:45 +0300

    Fixed naming.

--- AuctionExplorer/WeatherForecast.cs
+++ AuctionExplorer/WeatherForecast.cs
@@ -4,7 +4,7 @@ namespace AuctionExplorer
 {
     public class WeatherForecast
     {
-        public int pieklajigsID { get; set; }
+        public int Id { get; set; }
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
