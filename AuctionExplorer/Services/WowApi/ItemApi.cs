55bb563722f55758fe2283883393d4adb02b72d3
sendijs-dz <sendijs.dzenitis@gmail.com>
2020-08-03 20:41:19 +0300

    * Jesķ saport predmetov.

---
+++ AuctionExplorer/Services/WowApi/ItemApi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using AuctionExplorer.Models;
+
+namespace AuctionExplorer.Services.WowApi
+{
+    public class ItemApi
+    {
+        public ItemApi(WowApiBase wowApiBase)
+        {
+            WowApiBase = wowApiBase;
+        }
+
+        private WowApiBase WowApiBase { get; }
+
+        public async Task<GameItem> GetItem(int id, string nspace = "static-eu")
+        {
+            var requestedItem = await WowApiBase.WarcraftClient.GetItemAsync(id, nspace);
+
+            if (requestedItem.Success)
+            {
+                return new GameItem()
+                {
+                    Id = requestedItem.Value.Id,
+                    Name = requestedItem.Value.Name
+                };
+            }
+
+            throw new Exception($"Failed to get item with id {id}");
+        }
+    }
+}
--- AuctionExplorer/Services/WowApi/WowApiBase.cs
+++ AuctionExplorer/Services/WowApi/WowApiBase.cs
@@ -7,11 +7,14 @@ namespace AuctionExplorer.Services.WowApi
         internal WarcraftClient WarcraftClient { get; }
 
         public AchievementApi Achievement { get; private set; }
+        
+        public ItemApi Item { get; private set; }
 
         public WowApiBase(string clientId, string clientSecret)
         {
             WarcraftClient = new WarcraftClient(clientId, clientSecret);
             Achievement = new AchievementApi(this);
+            Item = new ItemApi(this);
         }
     }
 }
