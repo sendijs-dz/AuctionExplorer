124f2d8b7126d6fb002db07c3bd973203c02f41f
norbert-k <kakste.norberts@gmail.com>
2020-08-03 19:50:31 +0300

    Added support for Kelvin to WeatherForecast

--- AuctionExplorer/WeatherForecast.cs
+++ AuctionExplorer/WeatherForecast.cs
@@ -11,6 +11,8 @@ namespace AuctionExplorer
 
         public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
 
+        public int TemperatureK => TemperatureC + 383;
+
         public string Summary { get; set; }
     }
 }
